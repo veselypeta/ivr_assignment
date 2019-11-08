cf9376f20c78f232bd9c3e6497d3e16330b3bad6
veselypeta <veselypeta@gmail.com>
2019-11-08 09:50:31 +0000

    adds not tested detect angles

--- src/detect_positions.py
+++ src/detect_positions.py
@@ -20,6 +20,9 @@ class ProcessImages:
         self.image_1_joints = np.zeros((4, 2))
         self.image_2_joints = np.zeros((4, 2))
 
+        self.joint_positions = []
+        self.angles = []
+
     def callback2(self, data):
         self.image_2_joints = np.array(data.data).reshape((4, 2))
         print("cb2 called")
@@ -28,13 +31,53 @@ class ProcessImages:
         self.image_1_joints = np.array(data.data).reshape((4, 2))
         print("db1 called")
 
+    # assumption
+    # We receive x,z from image1
+    # we receive y,z from image2
+
+    def get_joint_position(self):
+        joint_positions = []
+        for i in range(self.image_1_joints.shape[0]):
+            xz_plane = self.image_1_joints[i]
+            yz_plane = self.image_2_joints[i]
+            x = xz_plane[0]
+            y = yz_plane[0]
+            z = (xz_plane[1] + yz_plane[1]) / 2
+            joint_positions.append([x, y, z])
+        self.joint_positions = np.array(joint_positions)
+
+    # now we have 3 angles to detect
+    # xy, xz, yz
+    def detect_joint_angles(self):
+        try:
+            xs = self.joint_positions[:, 0]
+            ys = self.joint_positions[:, 1]
+            zs = self.joint_positions[:, 2]
+
+            angles = []
+            for i in range(len(xs) - 1):
+                xy = np.arctan2(ys[i] - ys[i + 1], xs[i] - xs[i + 1])
+                xz = np.arctan2(zs[i] - zs[i + 1], xs[i] - xs[i + 1]) - xy
+                yz = np.arctan2(ys[i] - ys[i + 1], zs[i] - zs[i + 1]) - xy - xz
+                angles.append([xy, xz, yz])
+
+            self.angles = np.array(angles)
+        except IndexError:
+            print("Error happened")
+            return
+
+    def update(self):
+        self.get_joint_position()
+        self.detect_joint_angles()
+        print(self.angles)
+
 
 # call the class
 def main(args):
     ic = ProcessImages()
     try:
-        print("started the process")
-        rospy.spin()
+        while not rospy.is_shutdown():
+            ic.update()
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
